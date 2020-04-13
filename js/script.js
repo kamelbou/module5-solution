61955cefd5ac105ed0686b3e64252927c2ef5ed2
Kamel Bouakeur <kamelbouakeur@macbook-pro-de-kamel.home>
2020-04-13 14:05:03 +0200

    update

--- js/script.js
+++ js/script.js
@@ -118,7 +118,7 @@ function buildAndShowHomeHTML (categories) {
       //
       // var homeHtmlToInsertIntoMainPage = ....
       html =
-      insertProperty(homeHtmlUrl, "randomCategoryShortName", $`'{chosenCategoryShortName}'`);
+      insertProperty(homeHtmlUrl, "randomCategoryShortName", JSON.stringify(chosenCategoryShortName));
 
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
